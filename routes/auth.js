b90eca5c2868426306e0eb96a9344bc85fe12ba0
Moophie <michael.van.lierde@hotmail.com>
2021-05-12 15:54:10 +0200

    feat: add Passport plugin and user model

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var authController = require('../controllers/auth');
 
-router.get('/login',  authController.showLogin);
-router.post('/login',  authController.handleLogin);
-
 router.get('/signup',  authController.showSignup);
 router.post('/signup',  authController.handleSignup);
+
+router.get('/login',  authController.showLogin);
+router.post('/login',  authController.handleLogin);
 
 module.exports = router;
